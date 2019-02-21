61726e1134adbad53be14ff9a03ef1d1afb92a55
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-02-21 15:18:46 +0100

    Ajout d'une gestion d'erreur sur le fichier

--- main.py
+++ main.py
@@ -3,33 +3,28 @@ import os
 TODO = "todo"
 DONE = "done"
 PROMPT = ""
+INPUT_ERROR = "Invalid input"
 
 FILENAME = "data"
 
 MARK = "M"
 RESET = "R"
 ADD_CHAR = "A"
+DELETE = "D"
 QUIT = "Q"
 NO = "N"
+EMPTY = ""
+YES = "Y"
+NO = "N"
 
-LIST_CHOICES = [ADD_CHAR, MARK, RESET, QUIT]
-RESET_CHOICES = ["", "Y", "N"]
-YES_CHOICE = ["", "Y"]
+NAME_COL = 0
+SEP_COL = 1
+VERB_COL = 2
+LINE_LENGTH = 3
 
-def ask_for_reset():
-    print("No character left, want to reset ? Y/n")
-    while True:
-        choice = input(PROMPT)
-        if choice.upper() in RESET_CHOICES:
-            break
-    if choice.upper() in YES_CHOICE:
-        print("OK")
-    elif choice.upper() == NO:
-        print("NO")
-
-def add_character(name):
-    with open(FILENAME, "w") as file:
-        file.write(name + " : " + TODO)
+LIST_CHOICES = [ADD_CHAR, DELETE, MARK, RESET, QUIT]
+RESET_CHOICES = [EMPTY, YES, NO]
+YES_CHOICE = [EMPTY, YES]
 
 def reset():
     string = open(FILENAME).read()
@@ -37,21 +32,81 @@ def reset():
     with open(FILENAME, "w") as file:
         file.write(string)
 
+def remove_from_file(name):
+    print(name)
+    lines = open(FILENAME, "r").readlines()
+    with open(FILENAME, "w") as outfile:
+        for line in lines:
+            if name not in line:
+                outfile.write(line)
+
+def ask_for_reset():
+    print("No character left, want to reset ? Y/n")
+    while True:
+        choice = input(PROMPT)
+        if choice.upper() in RESET_CHOICES:
+            break
+        print(INPUT_ERROR)
+    if choice.upper() in YES_CHOICE:
+        reset()
+    elif choice.upper() == NO:
+        return QUIT
+    return QUIT
+
+def add_character():
+    print("Please enter a name for a new character :")
+    print("\t(Naming it q or Q will quit)")
+    name = input(PROMPT)
+    if name.upper() != QUIT:
+        with open(FILENAME, "a") as file:
+            file.write(name + " : " + TODO + os.linesep)
+    return name.upper()
+
+def delete_character():
+    print("Please select a character to delete :")
+    list_char = get_all()
+    print(list_char[0])
+    print("\t" + QUIT + ") Quit")
+    while True:
+        num = input(PROMPT)
+        if not num.isdigit():
+            if num.upper() == QUIT:
+                break
+        elif num.isdigit():
+            if 0 <= int(num) < list_char[1]:
+                break
+        print(INPUT_ERROR)
+    if num.isdigit():
+        name = list_char[0].split("\t")
+        name = [x for x in name if x != ""]
+        remove_from_file(name[int(num)].split()[1])
+    return num
+
 def sort_lines():
     lines = open(FILENAME, "r").readlines()
     with open(FILENAME, "w") as out_file:
-        for line in sorted(lines, key=lambda line: line.split(" ")[0], reverse=True):
+        for line in sorted(lines, key=lambda line: line.split(" ")[NAME_COL], reverse=True):
             out_file.write(line + os.linesep)
 
 def get_todos():
     cpt = 0
     lines = open(FILENAME, "r").readlines()
-    output = ""
+    output = EMPTY
     for line in lines:
         if TODO in line:
-            output += "\t" + str(cpt) + ") " + line.split()[0] + os.linesep
+            output += "\t" + str(cpt) + ") " + line.split()[NAME_COL] + "\t"
             cpt += 1
-    return (output, cpt)
+    return (output.rstrip(), cpt)
+
+def get_all():
+    cpt = 0
+    lines = open(FILENAME, "r").readlines()
+    output = EMPTY
+    for line in lines:
+        if len(line.split()) == LINE_LENGTH:
+            output += "\t" + str(cpt) + ") " + line.split()[NAME_COL] + "\t"
+            cpt += 1
+    return (output.rstrip(), cpt)
 
 def complete_character(char_name):
     char_todo = char_name + " : " + TODO
@@ -67,31 +122,64 @@ def complete_char_from_num(number, string):
         if number in line:
             complete_character(line.split()[1])
 
+def check_file():
+    if not os.path.exists(FILENAME):
+        with open(FILENAME, "w+"): pass
+    lines = open(FILENAME).readlines()
+    for line in lines:
+        if len(line.split()) != LINE_LENGTH:
+            print("FILE ILL FORMED\nPress any key...")
+            input("")
+            exit()
+    file = open(FILENAME).read()
+    with open(FILENAME, "w") as out:
+        out.write(file.rstrip() + "\n")
+
 def ask_complete():
-    print("Here is the list of remaining characters : " + os.linesep)
+    print("Here is the list of remaining characters : ")
     print(get_todos()[0])
     print("What do you want to do ?")
-    print("\tD) Mark one as DONE")
-    print("\tR) Reset characters")
-    print("\tQ) Quit")
+    print("\t" + ADD_CHAR + ") Add a new character in file", end="\t")
+    print("\t" + DELETE + ") Delete a character", end="\t")
+    print("\t" + MARK + ") Mark a character as DONE", end="\t")
+    print("\t" + RESET + ") Reset characters", end="\t")
+    print("\t" + QUIT+  ") Quit")
     while True:
         choice = input(PROMPT)
         if choice.upper() in LIST_CHOICES:
             break
-    if choice.upper() == "D":
+        print(INPUT_ERROR)
+    if choice.upper() == ADD_CHAR:
+        return add_character()
+    if choice.upper() == DELETE:
+        return delete_character()
+    elif choice.upper() == MARK:
         characters = get_todos()
-        print("\tQ) Quit")
         print(characters[0])
+        print("\t" + QUIT + ") Quit")
         print("Now choose one character :")
         while True:
             char = input(PROMPT)
-            if char.upper() == "Q" or  0 <= int(char) < characters[1]:
+            if char.upper() == QUIT or  0 <= int(char) < characters[1]:
                 break
+            print(INPUT_ERROR)
         if char.isdigit():
             complete_char_from_num(char, characters[0])
+            return char
+    elif choice.upper() == RESET:
+        reset()
+        return choice
+    return QUIT
 
 if __name__ == "__main__":
-    if get_todos()[1] == 0:
-        ask_for_reset()
-    else:
-        ask_complete()
+    print("Hello ! This program is not case sensitive.")
+    exit_var = EMPTY
+    while exit_var != QUIT:
+        check_file()
+        if os.stat(FILENAME).st_size == 0:
+            print("This file is empty please add a Character")
+            exit_var = add_character()
+        elif get_todos()[1] == 0:
+            exit_var = ask_for_reset()
+        else:
+            exit_var = ask_complete()
